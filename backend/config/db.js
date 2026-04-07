6df8a5cd8352328d9ff13ad1b8d1194a9bd6af2e
karan2857 <karan12395528@gmail.com>
2026-04-07 13:00:33 +0530

    commit changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,37 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: process.env.HOST,
+  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_DATABASE,
+
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+
+  ssl: {
+    ca: fs.readFileSync(
+      path.join(__dirname, '../certs/ca.pem')
+    ),
+  },
 });
+
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("✅ Database connected successfully");
+
+    connection.release(); 
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+  }
+})();
 
 module.exports = pool;
